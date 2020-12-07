607c726bb3a04c893a7668e8e9e8f44b23d30c9b
Andrew Blance <andrewblance@live.co.uk>
2020-12-07 10:35:04 +0000

    day seven

--- src/weekone.py
+++ src/weekone.py
@@ -18,6 +18,49 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+class DaySeven:
+    def import_bags(self, loc: str) -> List[str]:
+        data = open(loc).read().split("\n")
+        return data
+
+    def dict_maker(self, data: List[str]) -> Dict:
+        rules = {}
+        for x in data:
+            if x == "":
+                continue
+            bag_split = x.split(" contain ")
+            bag = bag_split[0].replace(" bags", "")
+            rule = bag_split[1].strip('.').split(', ')
+            rules[bag] = [x.replace(' bags', '').replace(' bag', '') for x in rule]
+        return rules
+
+    def gold_recurs(self, rules: Dict, search: str) -> int:
+        total = 0
+        for x in list(rules):
+            if any(search in bag for bag in rules[x]) and not\
+               any("flag" in bag for bag in rules[x]):
+                rules[x].append("flag")
+                total += 1 + self.gold_recurs(rules, x)
+        return total
+
+    def count_recurs(self, rules: Dict, search: str) -> int:
+        total = 0
+        for x in rules[search]:
+            if x != "no other":
+                total += int(x[0]) * (1 + self.count_recurs(rules, x[2:]))
+        return total
+
+    def answers(self, L: List[str]) -> None:
+        D = self.dict_maker(L)
+        total = self.gold_recurs(D, "shiny gold")
+        beeg_total = self.count_recurs(D, "shiny gold")
+        print(f"{bcolors.UNDERLINE}Day Six.{bcolors.ENDC}")
+        print("The number of bags is {}"
+              .format(str(total)))
+        print("The number of bags i have to carry is {}"
+              .format(str(beeg_total)))
+
+
 class DaySix:
     def import_answers(self, loc: str) -> List[str]:
         data = open(loc).read().split("\n\n")
@@ -493,8 +536,11 @@ def main():
     boarding = DayThree().import_map("src/data/boaring_passes.txt")
     DayFive().answers(boarding)
 
-    answers = DaySix().import_answers("src/data/questions_new.txt")
+    answers = DaySix().import_answers("src/data/questions.txt")
     DaySix().answers(answers)
+
+    bags = DaySeven().import_bags("src/data/bags.txt")
+    DaySeven().answers(bags)
 
 
 if __name__ == '__main__':
